b6f4b8e3d2ce3a650e30c387257fb8c632ee0964
martinvpeluffo <69002124+martinvpeluffo@users.noreply.github.com>
2020-08-16 19:49:08 -0300

    Login agosto/16 19:50
    init.js 43-48
    login.js 5-11
    login.html 41-60

--- js/login.js
+++ js/login.js
@@ -2,5 +2,10 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    document.getElementById('userinfo').addEventListener('submit',(evento) => {
+        evento.preventDefault();
+        window.location.href = 'index.html';
+        sessionStorage.setItem('logueado', true);
+        return true;
+    })
 });
